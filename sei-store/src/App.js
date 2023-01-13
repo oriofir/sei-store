db9613d1b0e18d058329457b5dc085ef81d838e1
oriofir <97814056+oriofir@users.noreply.github.com>
2023-01-12 18:08:55 -0800

    cart window opening on click

--- sei-store/src/App.js
+++ sei-store/src/App.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
+import Cart from "./Components/Cart/Cart";
+
 function App() {
+  const [cartOpen, setCartOpen] = useState(false);
   return (
     <div className="App">
       <header>
         <h1>SEIR Store</h1>
         <nav>
-          <button>
+          <button onClick={() => setCartOpen(!cartOpen)}>
             <img
               width="30"
               src="https://www.freepnglogos.com/uploads/shopping-cart-png/shopping-cart-svg-png-icon-download-28.png"
@@ -13,6 +17,7 @@ function App() {
           </button>
         </nav>
       </header>
+      <Cart cartOpen={cartOpen} setCartOpen={setCartOpen} />
     </div>
   );
 }
