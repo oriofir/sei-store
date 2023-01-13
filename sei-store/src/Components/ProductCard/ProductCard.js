8b3167a22e7575dd78ad3169f5604f90c0f76e7c
oriofir <97814056+oriofir@users.noreply.github.com>
2023-01-12 18:22:59 -0800

    created product card

---
+++ sei-store/src/Components/ProductCard/ProductCard.js
@@ -0,0 +1,23 @@
+// ProductCard.js
+// ProductCard.js
+
+function Product({ product }) {
+  return (
+    <li className="card">
+      <img src={`${product.image}`} alt="" />
+      <div className="description">
+        <h2>{product.title}</h2>
+        <div className={`price ${!!product.salePrice ? "sale" : ""}`}>
+          <span className="reg"> {`$${product.price}`}</span>
+          {product.salePrice && `$${product.salePrice}`}
+        </div>
+      </div>
+      <div className="quantity">
+        <button className="quantity-increase">+</button>
+        Quantity: 0<button className="quantity-decrease">-</button>
+      </div>
+    </li>
+  );
+}
+
+export default Product;
