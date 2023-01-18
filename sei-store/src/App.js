b6f32d432e0db68a0e179adbfc1fd23d58cc3b5d
oriofir <97814056+oriofir@users.noreply.github.com>
2023-01-18 14:18:07 -0800

    product card rendering and product object made

--- sei-store/src/App.js
+++ sei-store/src/App.js
@@ -1,5 +1,14 @@
 import { useState } from "react";
 import Cart from "./Components/Cart/Cart";
+import ProductCard from "./Components/ProductCard/ProductCard";
+
+const sampleProduct = {
+  title: "Small Bag",
+  image:
+    "https://res.cloudinary.com/everlane/image/upload/c_scale/c_fill,dpr_2.0,f_auto,g_face:center,h_422,q_auto,w_auto:26:338/v1/i/4ab043d5_1b24.jpg",
+  price: 10,
+  salePrice: 5,
+};
 
 function App() {
   const [cartOpen, setCartOpen] = useState(false);
@@ -18,6 +27,7 @@ function App() {
         </nav>
       </header>
       <Cart cartOpen={cartOpen} setCartOpen={setCartOpen} />
+      <ProductCard product={sampleProduct} />
     </div>
   );
 }
